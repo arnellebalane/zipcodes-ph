7b46ea5f99c03efb42e97c79078b83e51d47fd92
Arnelle Balane <arnellebalane@gmail.com>
2017-10-31 14:12:21 +0800

    add reverse method for reverse lookup

--- source/index.js
+++ source/index.js
@@ -3,5 +3,14 @@ const zipcodes = require('./zipcodes.json');
 module.exports = {
     find(zipcode) {
         return zipcodes[zipcode] || null
+    },
+    reverse(location) {
+        return Object.keys(zipcodes).find((zipcode) => {
+            const value = zipcodes[zipcode];
+            if (typeof value === 'string') {
+                return value === location;
+            }
+            return value.includes(location);
+        });
     }
 };
